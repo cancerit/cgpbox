b63337b9bd2f27e46505e7f19ca63f9414bd8965
Keiran Raine <keiranmraine@gmail.com>
2016-04-29 21:25:41 +0100

    run site from workspace

--- scripts/statusSrv.sh
+++ scripts/statusSrv.sh
@@ -42,7 +42,9 @@ run_parallel () {
   return $errors
 }
 
-do_parallel[progress]="progress.pl /datastore/output $NAME_MT $NAME_WT /opt/wtsi-cgp/site/data/progress.json >& ~/monitor.log"
-do_parallel[server]="cd /opt/wtsi-cgp/site && python -m SimpleHTTPServer 8000 >& ~/server.log"
+cp -r /opt/wtsi-cgp/site /datastore/site
+
+do_parallel[progress]="progress.pl /datastore/output $NAME_MT $NAME_WT /datastore/site/data/progress.json >& ~/monitor.log"
+do_parallel[server]="cd /datastore/site && python -m SimpleHTTPServer 8000 >& ~/server.log"
 
 run_parallel 2 do_parallel
