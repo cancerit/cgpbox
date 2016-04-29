c390a23d63c62df41d2c4dab0bb7dbd6b99cc105
Keiran Raine <keiranmraine@gmail.com>
2016-04-29 15:42:29 +0100

    Do a sensible paralle execution to prevent processes running forever after script ends

--- scripts/statusSrv.sh
+++ scripts/statusSrv.sh
@@ -2,7 +2,47 @@
 
 source /datastore/run.params
 
-progress.pl /datastore/output $NAME_MT $NAME_WT /opt/site/wtsi-cgp/data/progress.json >& ~/monitor.log&
+# about to do some parallel work...
+declare -A do_parallel
 
-cd /opt/wtsi-cgp/site
-python -m SimpleHTTPServer 8000 >& ~/server.log
+# declare function to run parallel processing
+run_parallel () {
+  # adapted from: http://stackoverflow.com/a/18666536/4460430
+  local max_concurrent_tasks=$1
+  local -A pids=()
+
+  for key in "${!do_parallel[@]}"; do
+    while [ $(jobs 2>&1 | grep -c Running) -ge "$max_concurrent_tasks" ]; do
+      sleep 1 # gnu sleep allows floating point here...
+    done
+    echo -e "\tStarting $key"
+    set -x
+    ${do_parallel[$key]} &
+    set +x
+    pids+=(["$key"]="$!")
+  done
+
+  errors=0
+  for key in "${!do_parallel[@]}"; do
+    pid=${pids[$key]}
+    local cur_ret=0
+    if [ -z "$pid" ]; then
+      echo "No Job ID known for the $key process" # should never happen
+      cur_ret=1
+    else
+      wait $pid
+      cur_ret=$?
+    fi
+    if [ "$cur_ret" -ne 0 ]; then
+      errors=$(($errors + 1))
+      echo "$key (${do_parallel[$key]}) failed."
+    fi
+  done
+
+  return $errors
+}
+
+do_parallel[progress]="progress.pl /datastore/output $NAME_MT $NAME_WT /opt/site/wtsi-cgp/data/progress.json >& ~/monitor.log"
+do_parallel[server]="cd /opt/wtsi-cgp/site;python -m SimpleHTTPServer 8000 >& ~/server.log"
+
+run_parallel 2 do_parallel
