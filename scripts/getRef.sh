b24a9ebfae5fe8d7439d375e45358a519bd2064f
Keiran Raine <keiranmraine@gmail.com>
2016-07-20 13:02:17 +0100

    Make sure restarts remove partial downloads

--- scripts/getRef.sh
+++ scripts/getRef.sh
@@ -6,7 +6,7 @@ SUCCESS="/datastore/$CGPBOX_VERSION/reference_files/unpack_ref.success"
 if [ -e $SUCCESS ]; then
   echo "Reference area already staged"
 else
-  rm -rf /datastore/$CGPBOX_VERSION/reference_files
+  rm -rf /datastore/$CGPBOX_VERSION/ref*
   mkdir -p /datastore/$CGPBOX_VERSION
   curl -sSL --retry 10 -o /datastore/$CGPBOX_VERSION/ref.tar.gz ftp://ftp.sanger.ac.uk/pub/cancer/cgpbox/ref-${CGPBOX_VERSION}.tar.gz
   tar -C /datastore/$CGPBOX_VERSION -zxf /datastore/$CGPBOX_VERSION/ref.tar.gz
