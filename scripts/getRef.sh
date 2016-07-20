9fbf6df2b7028f2ef68b630290641595bac8b267
Keiran Raine <keiranmraine@gmail.com>
2016-07-20 13:59:09 +0100

    Keep name on downloaded file

--- scripts/getRef.sh
+++ scripts/getRef.sh
@@ -2,14 +2,15 @@
 set -uxe
 
 SUCCESS="/datastore/$CGPBOX_VERSION/reference_files/unpack_ref.success"
+REFTAR="ref-${CGPBOX_VERSION}.tar.gz"
 
 if [ -e $SUCCESS ]; then
   echo "Reference area already staged"
 else
   rm -rf /datastore/$CGPBOX_VERSION/ref*
   mkdir -p /datastore/$CGPBOX_VERSION
-  curl -sSL --retry 10 -o /datastore/$CGPBOX_VERSION/ref.tar.gz ftp://ftp.sanger.ac.uk/pub/cancer/cgpbox/ref-${CGPBOX_VERSION}.tar.gz
-  tar -C /datastore/$CGPBOX_VERSION -zxf /datastore/$CGPBOX_VERSION/ref.tar.gz
+  curl -sSL --retry 10 -o /datastore/$CGPBOX_VERSION/$REFTAR ftp://ftp.sanger.ac.uk/pub/cancer/cgpbox/$REFTAR
+  tar -C /datastore/$CGPBOX_VERSION -zxf /datastore/$CGPBOX_VERSION/$REFTAR
   touch $SUCCESS
-  rm -f /datastore/$CGPBOX_VERSION/ref.tar.gz
+  rm -f /datastore/$CGPBOX_VERSION/$REFTAR
 fi
